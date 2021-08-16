846cae0e35a79fb9ca71fec0f2b1143741c0ddab
betich <betich365@gmail.com>
2021-08-16 22:46:04 +0700

    :wrench: fix private key env

--- src/helpers/firebase/certificate.ts
+++ src/helpers/firebase/certificate.ts
@@ -10,7 +10,7 @@ const cert = {
 
 const cert = {
 	projectId: process.env.PROJECT_ID,
-	privateKey: process.env.PRIVATE_KEY,
+	privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
 	clientEmail: process.env.CLIENT_EMAIL,
 };
 
