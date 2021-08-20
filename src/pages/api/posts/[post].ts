cf7b9cd712d25caaf038b3d2525b9438a4085367
betich <betich365@gmail.com>
2021-08-21 03:37:49 +0700

    :art: refactor api handlers

--- src/pages/api/posts/[post].ts
+++ src/pages/api/posts/[post].ts
@@ -1,101 +1,97 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
-import { firestore } from "@helpers/firebase/admin";
+import { firestore as db } from "@helpers/firebase/admin";
 import slugify from "slugify";
 import { authorizedMiddleware } from "@helpers/middleware";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
-handler
-	.get(async (req, res) => {
-		// get info about post
-		const { post } = req.query;
+handler.get(async (req, res) => {
+	// get info about post
+	const { post } = req.query;
 
-		const db = firestore;
-		await db
-			.collection("posts")
-			.where("slug", "==", post)
-			.get()
+	await db
+		.collection("posts")
+		.where("slug", "==", post)
+		.get()
+		.then((snapshot) => {
+			if (snapshot.empty) return res.status(404).json({ error: "post not found" });
+
+			res.status(200).json({ ...snapshot.docs[0].data() });
+		})
+		.catch((error) => {
+			res.status(400).json({ error: `something went wrong: ${error}` });
+			return null;
+		});
+});
+
+handler.put(authorizedMiddleware, async (req, res) => {
+	// update a post
+	const { post } = req.query;
+
+	db.runTransaction((transaction) => {
+		const postRef = db.collection("posts").where("slug", "==", post);
+		return transaction
+			.get(postRef)
 			.then((snapshot) => {
 				if (snapshot.empty) return res.status(404).json({ error: "post not found" });
+				else {
+					const docToUpdate = { slug: snapshot.docs[0].data().slug, id: snapshot.docs[0].id };
+					const updateRef = db.collection("posts").doc(docToUpdate.id);
 
-				res.status(200).json({ ...snapshot.docs[0].data() });
+					const timestamp = Date.now(); // timestamp
+					const newDoc = {
+						...req.body,
+						slug: req.body.title ? slugify(req.body.title, { lower: true }) : docToUpdate.slug,
+						updated: timestamp,
+					};
+
+					transaction.update(updateRef, newDoc);
+					res.status(200).json(newDoc);
+				}
+			})
+			.then(() => {
+				console.log("update post: transaction finished successfully");
 			})
 			.catch((error) => {
+				console.error("update post: error in transaction: ", error);
 				res.status(400).json({ error: `something went wrong: ${error}` });
-				return null;
 			});
-	})
-	.put(authorizedMiddleware, async (req, res) => {
-		// update a post
-		const { post } = req.query;
-
-		const db = firestore;
-
-		db.runTransaction((transaction) => {
-			const postRef = db.collection("posts").where("slug", "==", post);
-			return transaction
-				.get(postRef)
-				.then((snapshot) => {
-					if (snapshot.empty) return res.status(404).json({ error: "post not found" });
-					else {
-						const docToUpdate = { slug: snapshot.docs[0].data().slug, id: snapshot.docs[0].id };
-						const updateRef = db.collection("posts").doc(docToUpdate.id);
-
-						const timestamp = Date.now(); // timestamp
-						const newDoc = {
-							...req.body,
-							slug: req.body.title ? slugify(req.body.title, { lower: true }) : docToUpdate.slug,
-							updated: timestamp,
-						};
-
-						transaction.update(updateRef, newDoc);
-						res.status(200).json(newDoc);
-					}
-				})
-				.then(() => {
-					console.log("update post: transaction finished successfully");
-				})
-				.catch((error) => {
-					console.error("update post: error in transaction: ", error);
-					res.status(400).json({ error: `something went wrong: ${error}` });
-				});
-		});
-	})
-	.delete(authorizedMiddleware, async (req, res) => {
-		// delete a post
-		const { post } = req.query;
-
-		const db = firestore;
-
-		db.runTransaction((transaction) => {
-			const postRef = db.collection("posts").where("slug", "==", post);
-			return transaction
-				.get(postRef)
-				.then((snapshot) => {
-					if (snapshot.empty) return res.status(404).json({ error: "post not found" });
-					else {
-						const deleteDoc = snapshot.docs[0];
-
-						db.collection("posts")
-							.doc(deleteDoc.id)
-							.delete()
-							.then(() => {
-								res.status(200).json({ message: "document successfully deleted" });
-							})
-							.catch((error) => {
-								res.status(400).json({ error: `something went wrong: ${error}` });
-							});
-					}
-				})
-				.then(() => {
-					console.log("delete post: transaction finished successfully");
-				})
-				.catch((error) => {
-					console.error("delete post: error in transaction: ", error);
-					res.status(400).json({ error: `something went wrong: ${error}` });
-				});
-		});
 	});
+});
+
+handler.delete(authorizedMiddleware, async (req, res) => {
+	// delete a post
+	const { post } = req.query;
+
+	db.runTransaction((transaction) => {
+		const postRef = db.collection("posts").where("slug", "==", post);
+		return transaction
+			.get(postRef)
+			.then((snapshot) => {
+				if (snapshot.empty) return res.status(404).json({ error: "post not found" });
+				else {
+					const deleteDoc = snapshot.docs[0];
+
+					db.collection("posts")
+						.doc(deleteDoc.id)
+						.delete()
+						.then(() => {
+							res.status(200).json({ message: "document successfully deleted" });
+						})
+						.catch((error) => {
+							res.status(400).json({ error: `something went wrong: ${error}` });
+						});
+				}
+			})
+			.then(() => {
+				console.log("delete post: transaction finished successfully");
+			})
+			.catch((error) => {
+				console.error("delete post: error in transaction: ", error);
+				res.status(400).json({ error: `something went wrong: ${error}` });
+			});
+	});
+});
 
 export default handler;
--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,73 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
-import { firestore } from "@helpers/firebase/admin";
+import { firestore as db } from "@helpers/firebase/admin";
 import slugify from "slugify";
 import { authenticatedMiddleware } from "@helpers/middleware";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
-handler
-	.get(async (req, res) => {
-		// get posts
-		const db = firestore;
-		await db
-			.collection("posts")
-			.get()
+handler.get(async (req, res) => {
+	// get posts
+	await db
+		.collection("posts")
+		.get()
+		.then((snapshot) => {
+			const postsData = snapshot.docs.map((post) => post.data());
+			res.status(200).json(postsData);
+		})
+		.catch((error) => {
+			res.status(400).json({
+				error: `something went wrong: ${error}`,
+			});
+		});
+});
+
+handler.post(authenticatedMiddleware, async (req, res) => {
+	// create a post
+	// has to be logged in & curr uid has to match the new
+	const { title } = req.body;
+	const slug = slugify(title, { lower: true });
+
+	db.runTransaction((transaction) => {
+		const postsRef = db.collection("posts");
+		return transaction
+			.get(postsRef)
 			.then((snapshot) => {
+				// find and create a new post
 				const postsData = snapshot.docs.map((post) => post.data());
-				res.status(200).json(postsData);
+
+				if (postsData.some((post) => post.slug === slug))
+					res.status(409).json({
+						error: "a post with that name already exists",
+					});
+				else {
+					const timestamp = Date.now(); // timestamp
+					const requestData = {
+						...req.body,
+						slug,
+						created: timestamp,
+						uid: req.uid,
+					};
+
+					const newPostRef = db.collection("posts").doc();
+
+					transaction.set(newPostRef, requestData);
+					res.status(200).json(requestData);
+				}
+			})
+			.then(() => {
+				console.log("add post: transaction finished successfully");
 			})
 			.catch((error) => {
+				console.error("posts: error in transaction: ", error);
 				res.status(400).json({
 					error: `something went wrong: ${error}`,
 				});
 			});
-	})
-	.post(authenticatedMiddleware, async (req, res) => {
-		// create a post
-		// has to be logged in & curr uid has to match the new
-		const { title } = req.body;
-		const slug = slugify(title, { lower: true });
-
-		const db = firestore;
-
-		db.runTransaction((transaction) => {
-			const postsRef = db.collection("posts");
-			return transaction
-				.get(postsRef)
-				.then((snapshot) => {
-					// find and create a new post
-					const postsData = snapshot.docs.map((post) => post.data());
-
-					if (postsData.some((post) => post.slug === slug))
-						res.status(409).json({
-							error: "a post with that name already exists",
-						});
-					else {
-						const timestamp = Date.now(); // timestamp
-						const requestData = {
-							...req.body,
-							slug,
-							created: timestamp,
-							uid: req.uid,
-						};
-
-						const newPostRef = db.collection("posts").doc();
-
-						transaction.set(newPostRef, requestData);
-						res.status(200).json(requestData);
-					}
-				})
-				.then(() => {
-					console.log("add post: transaction finished successfully");
-				})
-				.catch((error) => {
-					console.error("posts: error in transaction: ", error);
-					res.status(400).json({
-						error: `something went wrong: ${error}`,
-					});
-				});
-		});
 	});
+});
 
 export default handler;
